5ea0c6c80b13a2f6aa2353497f6b2179b105c070
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-03-23 22:45:41 -0300

    feat: Adiciona o componente Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,41 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
 import './header.css';
 
 function Header() {
   return (
-    <Navbar expand="lg">
-      <header>Header</header>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="/"><img src={Logo}></img></Navbar.Brand> 
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Link to="/" className="nav-link">Home</Link>
+            <Link to="/produtos" className="nav-link">Produtos</Link>
+            <Link className="nav-link">Destaque</Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Digite o produto..."
+              aria-label="Search"
+            />
+            <Button><span className="fas fa-carrot"></span>Buscar</Button>
+            <div className='fas fa-shopping-cart'></div>
+            <div className='fas fa-envelope'></div>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './reset.css';
+import './index.css';
 import './variables.css'
 import App from './App';
 
